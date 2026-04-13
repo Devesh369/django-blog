bdbb9b141081996eca3c9af708a8eb1b440299b7
Devesh Patel <deveshpatel@Deveshs-MacBook-Air.local>
2026-04-13 21:33:30 +0530

    posts by cat

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from blogs.models import Category , Blog
 
 
 
 
 def home(request):
-    return render(request, 'home.html')
+    
+    categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured = True).order_by('updated_at')
+    posts = Blog.objects.filter(is_featured =False, status = 'Published')
+
+
+    context = {'categories' : categories,
+               'featured_posts':featured_posts,
+               'posts':posts,
+
+               }
+    return render(request, 'home.html', context)
+
+
+def page_not_found(request, exception):
+    return render(request, '404.html', status=404)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404 , redirect
+from django.http import HttpResponse 
+from .models import Blog , Category
 
-# Create your views here.
+
+def posts_by_category(request,category_id):
+    #fetch the posts that belog to the category with id = category_id
+    posts = Blog.objects.filter(status='Published', category = category_id)
+   
+    context = {
+        'posts':posts,
+       
+    }
+    return render(request, 'posts_by_category.html', context)
+    
