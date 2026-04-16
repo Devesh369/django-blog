8202e9412e1d5ce964f231eefe7a1841c1e474ca
Devesh Patel <deveshpatel@Deveshs-MacBook-Air.local>
2026-04-16 16:52:04 +0530

    posts crud op.

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render , redirect
 from blogs.models import Category , Blog
 from  django.contrib.auth.decorators import login_required
-from .forms import CategoryForm
+from .forms import CategoryForm , PostForm
 from django.shortcuts import get_object_or_404
+from django.template.defaultfilters import slugify
 
 # Create your views here.
 
@@ -54,3 +55,61 @@ def delete_category(request , pk):
     cat = get_object_or_404(Category , pk = pk)
     cat.delete()
     return redirect('categories')
+
+
+# POST CRUD
+
+def posts(request):
+    posts = Blog.objects.all()
+    context = {
+        'posts':posts,
+    }
+    return render (request , 'dashboard/posts.html',context)
+
+def add_posts(request):  # for image insert must add enctype in form and here request.FILES.
+    if request.method == "POST":
+        form = PostForm(request.POST , request.FILES)
+        if form.is_valid():
+            post = form.save(commit = False) #temp saving form/ gives time to use tabel data
+            post.author =  request.user
+            post.save()
+            title = form.cleaned_data['title']
+            post.slug = slugify(title)+ '-'+str(post.id)
+            post.save() 
+            return redirect('posts')
+        else:
+            print("form is invalid")
+            print(form.errors)
+
+#this below code for render blank form on page
+    form = PostForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'dashboard/add_posts.html',context)
+
+
+
+def edit_posts(request , pk):
+    posts = get_object_or_404(Blog, pk = pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST , request.FILES ,  instance= posts ) 
+        if form.is_valid():
+            posts = form.save()
+            title = form.cleaned_data['title']
+            posts.slug = slugify(title)+'-'+str(posts.id)
+            posts.save()
+            return redirect('posts')
+
+    form = PostForm(instance=posts)
+    context = {
+        'form':form,
+        'post':posts,  #posts for when user click on update button to update particular id
+    }
+    return render(request, 'dashboard/edit_posts.html',context)
+
+
+def delete_posts(request, pk):
+    blog = get_object_or_404(Blog , pk = pk)
+    blog.delete()
+    return redirect('posts')
